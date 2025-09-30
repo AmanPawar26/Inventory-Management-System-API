6de8d8099017df51c498cefe1f94203c185250cc
Your Name <amanrpxx@gmail.com>
2025-09-30 13:46:48 +0530

    Controller logic for CRUD Endpoints for Products

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,76 @@
+import db from '../db/db.js'
+
+// Get All Properties (GET)
+
+export const getAllProducts = (req, res) => {
+    db.all('SELECT * FROM Products', (err, rows) => {
+        if(err){
+            console.err('Database Read Error:', err);
+            return res.status(500).json({error: 'Failed to fetch Properties'})
+        }
+        res.json(rows)
+    })
+}
+
+export const createProducts = (req, res) => {
+    const {name, description, stock_quantity} = req.body;
+    const sql = `
+    INSERT INTO Products (
+    Name, Description, Stock_Quantity
+    ) VALUES (?, ?, ?)
+    `;
+    const params = [
+        name, description, stock_quantity
+    ];
+
+    db.run(sql, params, function (err) {
+        if(err){
+            console.error('DB Insert error:', err);
+            return res.status(500).json({error: 'Failed to create product'})
+        }
+        res.status(201).json({id: this.lastId, message: 'Product Created Successfully'})
+    });
+};
+
+export const updateProducts = (req, res) => {
+    const {id} = req.params;
+    const {name, description, stock_quantity} = req.body;
+
+    const sql = `
+    UPDATE Products SET
+    Name = ?,
+    Description = ?,
+    Stock_Quantity = ?
+    WHERE id = ?
+    `;
+     const params = [
+        name, description, stock_quantity, id
+    ];
+    db.run(sql, params, function (err){
+        if(err){
+            console.error('DB Update Error:', err);
+            return res.status(500).json({error: 'Failed to update product'})
+        }
+        if(this.changes === 0) {
+           return res.status(404).json({error: 'Product not found'})
+        }
+        res.json({message: 'Product Updated', changes: this.changes
+        })
+    });
+};
+
+export const deleteProducts = (req, res) => {
+    const {id} = req.params;
+    db.run('DELETE FROM Products WHERE id = ?', [id],
+        function(err){
+            if(err){
+                console.error('DB Delete Error:', err);
+                return res.status(500).json({error: 'Failed to Delete Product'})
+            }
+            if(this.changes === 0){
+                return res.status(400).json({error: 'Product not found'})
+            }
+            res.json({message: 'Property deleted', changes: this.changes});
+        });
+};
+
