edfcbcf9117da13bc328d471be4ed0dbc431c8f5
Your Name <amanrpxx@gmail.com>
2025-09-30 15:27:32 +0530

    Fixed Indentation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import productRoutes from './routes/products.routes.js'
+import manageStockRoutes from './routes/managestock.routes.js'
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', productRoutes);
+app.use('/api', manageStockRoutes)
+
 
 export default app;
