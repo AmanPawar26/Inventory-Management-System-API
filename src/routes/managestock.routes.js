2940a8cfdaa1aa612e7b39e79dd77f2f4bc5be5e
Your Name <amanrpxx@gmail.com>
2025-10-01 12:29:11 +0530

    Added the route (endpoint)  to list all products currently below the set threshold

--- src/routes/managestock.routes.js
+++ src/routes/managestock.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {decreaseStockQuantity, increaseStockQuantity} from "../controllers/managestock.controller.js"
+import {decreaseStockQuantity, increaseStockQuantity, productsBelowThreshold} from "../controllers/managestock.controller.js"
 
 const router = express.Router();
 
 router.put('/products/:id/increase', increaseStockQuantity);
 router.put('/products/:id/decrease', decreaseStockQuantity)
+router.get('/products/low-stock', productsBelowThreshold);
 
 export default router;
