d35461e6e0374b5b4a1f3cb88ab741b9fceda51a
Your Name <amanrpxx@gmail.com>
2025-10-02 14:22:20 +0530

    Added route to get a product by id

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {getAllProducts, createProducts, updateProducts, deleteProducts} from '../controllers/products.controller.js';
+import {getAllProducts, createProducts, updateProducts, deleteProducts, getProductById} from '../controllers/products.controller.js';
 
 const router = express.Router();
 
 router.get('/products', getAllProducts);
+router.get('/products/:id', getProductById)
 router.post('/products', createProducts);
 router.put('/products/:id', updateProducts);
 router.delete('/products/:id', deleteProducts)
